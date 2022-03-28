959b1444b6299b4e46992d56f4f9c7e1e6437bfb
zhangyi <2510906118@qq.com>
2022-03-28 22:10:32 +0800

    整理头条部分

--- blog-vue/src/components/home_page/news.vue
+++ blog-vue/src/components/home_page/news.vue
@@ -1,61 +1,36 @@
 <template>
   <div id="news">
     <div id="news-container">
-      <div class="lift_one_div">
-        <div class="lift_one_div_title">
-          <svg
-            t="1640591805292"
-            class="icon"
-            viewBox="0 0 1024 1024"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            p-id="891"
-            width="32"
-            height="32"
-          >
-            <path
-              d="M512 510.3104m-450.816 0a450.816 450.816 0 1 0 901.632 0 450.816 450.816 0 1 0-901.632 0Z"
-              fill="#E9F4FF"
-              p-id="892"
-            ></path>
-            <path
-              d="M691.456 216.7296h-358.912c-41.9328 0-75.8784 33.9968-75.8784 75.8784v472.1664c0 28.5696 29.696 47.36 55.552 35.1744l167.168-78.8992c20.48-9.6768 44.2368-9.6768 64.7168-0.0512l167.7824 79.0016c25.8048 12.1344 55.5008-6.656 55.5008-35.2256V292.6592c0-41.9328-33.9968-75.9296-75.9296-75.9296z m-65.8944 341.6064H398.4384c-15.0016 0-27.136-12.1344-27.136-27.136 0-15.0016 12.1344-27.136 27.136-27.136h227.1232c15.0016 0 27.136 12.1344 27.136 27.136 0 15.0016-12.1344 27.136-27.136 27.136z m0-123.8528H398.4384c-15.0016 0-27.136-12.1344-27.136-27.136 0-15.0016 12.1344-27.136 27.136-27.136h227.1232c15.0016 0 27.136 12.1344 27.136 27.136 0 15.0016-12.1344 27.136-27.136 27.136z"
-              fill="#2595E8"
-              p-id="893"
-            ></path>
-            <path
-              d="M767.3856 481.536V292.6592c0-41.9328-33.9968-75.8784-75.8784-75.8784H332.544c-41.9328 0-75.8784 33.9968-75.8784 75.8784v450.9696A451.56864 451.56864 0 0 0 353.28 754.0224c22.7328 0 45.0048-1.6896 66.816-4.9152l59.2896-28.0064c16.7936-7.936 35.6864-9.2672 53.248-4.1984 105.0624-45.6704 189.3888-130.1504 234.752-235.3664z m-141.824 76.8H398.4384c-15.0016 0-27.136-12.1344-27.136-27.136s12.1344-27.136 27.136-27.136h227.1232c15.0016 0 27.136 12.1344 27.136 27.136s-12.1344 27.136-27.136 27.136z m0-123.8528H398.4384c-15.0016 0-27.136-12.1344-27.136-27.136 0-15.0016 12.1344-27.136 27.136-27.136h227.1232c15.0016 0 27.136 12.1344 27.136 27.136 0 15.0016-12.1344 27.136-27.136 27.136z"
-              fill="#3A9CED"
-              p-id="894"
-            ></path>
-            <path
-              d="M405.76 558.336h-7.3216c-15.0016 0-27.136-12.1344-27.136-27.136s12.1344-27.136 27.136-27.136h101.5808a454.7072 454.7072 0 0 0 73.8304-69.5808H398.4384c-15.0016 0-27.136-12.1344-27.136-27.136 0-15.0016 12.1344-27.136 27.136-27.136h214.9376a448.4352 448.4352 0 0 0 61.3376-163.4816H332.544c-41.9328 0-75.8784 33.9968-75.8784 75.8784v300.4416a446.86848 446.86848 0 0 0 149.0944-34.7136z"
-              fill="#59ADF8"
-              p-id="895"
-            ></path>
-            <path
-              d="M502.4256 216.7808H332.544c-41.9328 0-75.8784 33.9968-75.8784 75.8784V412.16c103.3728-33.792 190.3616-103.936 245.76-195.3792z"
-              fill="#6BC2FC"
-              p-id="896"
-            ></path>
-          </svg>
-          <h4 style="margin-top: 5px">头条</h4>
+      
+      <div class="flex_colum">
+        <div class="flex_row">
+          <div class="flex_colum_center">
+            <svg-icon icon-class="top_hot" ></svg-icon>
+          </div>
+          <div class="flex_colum_center">
+            <h4>头条</h4>
+          </div>
         </div>
-        <div class="lift_one_div_img">
+
+        <div class="flex_row_center">
           <img
             src="@/assets/csdn_copy_logo.jpg"
-            style="height: 150px; width: 350px"
           />
         </div>
-        <div class="lift_one_div_news_title">
+
+        <div>
           <h4>
             <a :href="jump_url">{{ lift_one_div_news_title }}</a>
           </h4>
         </div>
-        <div class="lift_one_div_news_context">
+
+        <div class="hot_top_funt_style">
           {{ lift_one_div_news_context }}
         </div>
+      
       </div>
+
+
 
       <div class="lift_two_div">
         <div class="paging">
@@ -223,6 +198,33 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+/* 水平排列 */
+.flex_row{
+  display: flex;
+  flex-direction: row;
+}
+
+/* 竖排列 */
+.flex_colum{
+  display: flex;
+  flex-direction: column;
+}
+
+/* 水平居中 */
+.flex_row_center{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+/* 垂直居中 */
+.flex_colum_center{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
 #news {
   margin-top: 80px;
   min-width: 1445px;
@@ -235,28 +237,8 @@ export default {
   height: 300px;
 }
 
-.lift_one_div {
-  width: 450px;
-  display: flex;
-  flex-direction: column;
-}
-
-.lift_one_div_title {
-  display: flex;
-  flex-direction: row;
-}
-
-.lift_one_div_img {
-  display: flex;
-  justify-content: center;
-}
-
-.lift_one_div_news_title {
-  margin-top: 20px;
-}
-
-.lift_one_div_news_context {
-  margin-top: 10px;
+/* 头条内容简介字体样式 */
+.hot_top_funt_style {
   font-size: 15px;
   color: silver;
 }
